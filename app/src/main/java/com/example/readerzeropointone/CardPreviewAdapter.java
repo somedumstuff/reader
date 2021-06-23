3dd1f14f5843be25c7593c7e77259387a9f13d06
Ahmad Fahim <ahmadfahim2k@gmail.com>
2021-06-23 18:19:18 +0530

    ArticleActivity works for 'Your Subs' now!
    Modified files:
     CardPreviewAdapter.java

--- app/src/main/java/com/example/readerzeropointone/CardPreviewAdapter.java
+++ app/src/main/java/com/example/readerzeropointone/CardPreviewAdapter.java
@@ -7,6 +7,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,9 +25,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.Picasso;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import io.realm.Realm;
 
@@ -77,27 +87,90 @@ public class CardPreviewAdapter  extends RecyclerView.Adapter<CardPreviewAdapter
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String url = daCardActivity.getCardPreviewLink();
+                ExecutorService executor = Executors.newSingleThreadExecutor();
+                Handler handler = new Handler(Looper.getMainLooper());
+                executor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Background work here
+                        StringBuilder article = new StringBuilder();
+                        String queryTag = "p";
+                        boolean noClasses = false;
 
-                Uri uri = Uri.parse(daCardActivity.getCardPreviewLink());
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                context.startActivity(intent);
-                CardDBHelper helper = new CardDBHelper(Realm.getDefaultInstance());
-                helper.addCard(daCardActivity.getCardPreviewHeadlineTextView(), daCardActivity.getCardPreviewSubtitleTextView(),
-                        daCardActivity.getCardPreviewTimeTextView(), daCardActivity.getCardPreviewAuthorTextView(), daCardActivity.getCardPreviewSiteLogoUrl(),
-                        daCardActivity.getCardPreviewArticleImageUrl(), daCardActivity.getCardPreviewLink());
-                adapter2.notifyDataSetChanged();
-//                int pos = holder.getAbsoluteAdapterPosition();
-//                DaCardActivity daCardActivity = (DaCardActivity) localArrayList.get(pos);
-//                Context context = holder.itemView.getContext();
-//                Intent intent = new Intent(context, ArticleActivity.class);
-//                intent.putExtra("AUTHOR", daCardActivity.getCardPreviewAuthorTextView());
-//                intent.putExtra("TIME", daCardActivity.getCardPreviewTimeTextView());
-//                intent.putExtra("SUBTITLE", daCardActivity.getCardPreviewSubtitleTextView());
-//                intent.putExtra("HEADLINE", daCardActivity.getCardPreviewHeadlineTextView());
-//                intent.putExtra("ARTICLE_IMAGE", daCardActivity.getCardPreviewArticleImageUrl());
-//                intent.putExtra("SITE_LOGO", daCardActivity.getCardPreviewSiteLogoUrl());
-//                context.startActivity(intent);
+                        if(url.contains("https://indianexpress.com"))
+                            noClasses = true;
+                        else if (url.contains("https://www.nytimes.com/"))
+                            queryTag = "p.css-axufdj.evys1bk0";
+                        else if (url.contains("https://www.politico.com/"))
+                            queryTag = "p.story-text__paragraph";
+                        else if (url.contains("https://www.latimes.com/"))
+                            noClasses = true;
+
+                        try {
+                            final Document document = Jsoup.connect(url).get();
+
+                            for(Element p : document.select(queryTag)) {
+                                if(!noClasses || p.className().equals("")){
+                                    article.append(p.text());
+                                    Log.v("para", queryTag + p.text());
+                                }
+                            }
+                            Log.v("para", url);
+                            Log.v("Jsoup", String.valueOf(document));
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                //UI Thread work here
+                                String articleText = article.toString();
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Context context = holder.itemView.getContext();
+                                        Intent intent = new Intent(context, ArticleActivity.class);
+                                        intent.putExtra("AUTHOR", daCardActivity.getCardPreviewAuthorTextView());
+                                        intent.putExtra("TIME", daCardActivity.getCardPreviewDate());
+                                        intent.putExtra("SUBTITLE", daCardActivity.getCardPreviewSubtitleTextView());
+                                        intent.putExtra("HEADLINE", daCardActivity.getCardPreviewHeadlineTextView());
+                                        intent.putExtra("ARTICLE_IMAGE", daCardActivity.getCardPreviewArticleImageUrl());
+                                        intent.putExtra("SITE_LOGO", daCardActivity.getCardPreviewSiteLogoUrl());
+                                        intent.putExtra("ARTICLE_LINK", daCardActivity.getCardPreviewLink());
+                                        intent.putExtra("ARTICLE_PARA", articleText);
+                                        context.startActivity(intent);
+                                    }
+                                }, 800);
+                            }
+                        });
+                    }
+                });
             }
+
+//            public void onClick(View v) {
+//
+//                Uri uri = Uri.parse(daCardActivity.getCardPreviewLink());
+//                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//                context.startActivity(intent);
+//                CardDBHelper helper = new CardDBHelper(Realm.getDefaultInstance());
+//                helper.addCard(daCardActivity.getCardPreviewHeadlineTextView(), daCardActivity.getCardPreviewSubtitleTextView(),
+//                        daCardActivity.getCardPreviewTimeTextView(), daCardActivity.getCardPreviewAuthorTextView(), daCardActivity.getCardPreviewSiteLogoUrl(),
+//                        daCardActivity.getCardPreviewArticleImageUrl(), daCardActivity.getCardPreviewLink());
+//                adapter2.notifyDataSetChanged();
+////                int pos = holder.getAbsoluteAdapterPosition();
+////                DaCardActivity daCardActivity = (DaCardActivity) localArrayList.get(pos);
+////                Context context = holder.itemView.getContext();
+////                Intent intent = new Intent(context, ArticleActivity.class);
+////                intent.putExtra("AUTHOR", daCardActivity.getCardPreviewAuthorTextView());
+////                intent.putExtra("TIME", daCardActivity.getCardPreviewTimeTextView());
+////                intent.putExtra("SUBTITLE", daCardActivity.getCardPreviewSubtitleTextView());
+////                intent.putExtra("HEADLINE", daCardActivity.getCardPreviewHeadlineTextView());
+////                intent.putExtra("ARTICLE_IMAGE", daCardActivity.getCardPreviewArticleImageUrl());
+////                intent.putExtra("SITE_LOGO", daCardActivity.getCardPreviewSiteLogoUrl());
+////                context.startActivity(intent);
+//            }
         });
     }
 
